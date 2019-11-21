a5a5202fb007923018e2e034797a9a70569dfc9c
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-11-21 15:26:40 +0100

    Activate restart button

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,11 @@
-var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
+var tempoInicial = $("#tempo-digitacao").text();
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-});
-
-$("#botao-reiniciar").click(function(){
-    campo.attr("disabled", false);
-    campo.val("");
-    $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
-    $("#tempo-digitacao").text(tempoInicial);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
 function atualizaTamanhoFrase() {
