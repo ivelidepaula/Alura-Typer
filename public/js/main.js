4bb0ec621f310131f690af556c28ccd5413aaff7
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-11-21 15:20:23 +0100

    Start word and character counters

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,17 @@ $("#botao-reiniciar").click(function(){
 
 function atualizaTamanhoFrase() {
     var frase = $(".frase").text();
-    var numPalavras  = frase.split(" ").length;
+    var numPalavras = frase.split(" ").length;
     var tamanhoFrase = $("#tamanho-frase");
     tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
 }
