7f1cb6c548a57b9aebc007a8269581d8b19bb951
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-11-21 15:18:08 +0100

    Update sentence length according to input

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,5 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
 var tempoInicial = $("#tempo-digitacao").text();
-
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-});
 
 var tempoRestante = $("#tempo-digitacao").text();
 campo.one("focus", function() {
@@ -34,3 +20,10 @@ $("#botao-reiniciar").click(function(){
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
 });
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras  = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
