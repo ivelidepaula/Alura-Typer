d3440b182f23af1cbf578f7311d65e6d647c108e
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-11-25 11:37:49 +0100

    Synchronize score

--- public/js/placar.js
+++ public/js/placar.js
@@ -82,3 +82,15 @@ function sincronizaPlacar(){
         });
     });
 }
+
+function atualizaPlacar(){
+    $.get("http://localhost:3000/placar",function(data){
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+
+            linha.find(".botao-remover").click(removeLinha);
+
+            $("tbody").append(linha);
+        });
+    });
+}
